a1bd732a74c0798f5e871a758ea8f70e674b64cf
pveentjer <alarmnummer@gmail.com>
2018-07-09 08:57:46 +0300

    Added to/from command line options

--- src/main/java/com/hazelcast/Metrics.java
+++ src/main/java/com/hazelcast/Metrics.java
@@ -1,32 +1,53 @@
 package com.hazelcast;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Metrics {
 
+    private final MetricsCli cli;
+
+    public Metrics(String[] args) {
+        cli = new MetricsCli(args);
+    }
+
     public static void main(String[] args) throws IOException {
-        File file = new File(args[1]);
-
-        if (!file.exists()) {
-            throw new IOException("Can't find file " + file);
+        for (String arg : args) {
+            System.out.println(arg);
         }
+        System.out.println("--------");
 
-        Map<String, StringBuffer> result = generate(file);
+        Metrics metrics = new Metrics(args);
+        metrics.render();
+
+        // write your code here
+    }
+
+    private void render() throws IOException {
+        Map<String, StringBuffer> result = generate(cli.inputFile());
 
         StringBuffer data = new StringBuffer();
         StringBuffer select = new StringBuffer();
         StringBuffer selectHandler = new StringBuffer();
 
         List<String> keys = new ArrayList<>(result.size());
-        Map<String,StringBuffer> sanitizedResult = new HashMap<>(result.size());
-        Map<String,String> keyMapping = new HashMap<>();
-        for(String key: result.keySet()){
+        Map<String, StringBuffer> sanitizedResult = new HashMap<>(result.size());
+        Map<String, String> keyMapping = new HashMap<>();
+        for (String key : result.keySet()) {
             String sanitizedKey = sanitize(key);
-            keyMapping.put(sanitizedKey,key);
+            keyMapping.put(sanitizedKey, key);
             keys.add(sanitizedKey);
             sanitizedResult.put(sanitizedKey, result.remove(key));
         }
@@ -35,21 +56,19 @@ public class Metrics {
 
         Collections.sort(keys);
 
-        for (String name: keys) {
+        for (String name : keys) {
 
-            data.append("var data_"+ name +" = google.visualization.arrayToDataTable([\n");
+            data.append("var data_" + name + " = google.visualization.arrayToDataTable([\n");
             data.append("['X', 'Y'],\n");
             data.append(result.get(name));
             data.append("]);\n");
 
-            select.append("   <option value=\"data_"+name+"\">"+escapeHTML(keyMapping.get(name))+"</option>\n");
+            select.append("   <option value=\"data_" + name + "\">" + escapeHTML(keyMapping.get(name)) + "</option>\n");
 
-            selectHandler.append("          if(selValue=='data_"+name+"'){x=data_"+name+";}\n");
+            selectHandler.append("          if(selValue=='data_" + name + "'){x=data_" + name + ";}\n");
         }
 
-        writeResult(data,select,selectHandler);
-
-        // write your code here
+        writeResult(data, select, selectHandler);
     }
 
 
@@ -70,26 +89,26 @@ public class Metrics {
 
     private static String sanitize(String name) {
         return name
-                .replace("/","_")
-                .replace(">","_")
-                .replace(".","_")
-                .replace("[","_")
-                .replace("]","_")
-                .replace("-","_")
-                .replace(":","_");
+                .replace("/", "_")
+                .replace(">", "_")
+                .replace(".", "_")
+                .replace("[", "_")
+                .replace("]", "_")
+                .replace("-", "_")
+                .replace(":", "_");
     }
 
-    private static void writeResult(StringBuffer data,StringBuffer select,StringBuffer selectHandler) throws IOException {
+    private void writeResult(StringBuffer data, StringBuffer select, StringBuffer selectHandler) throws IOException {
         String template = loadTemplate();
         String result = template.replace("$data", data);
         result = result.replace("$select", select);
-        result = result.replace("$handler",selectHandler);
-        try (PrintStream out = new PrintStream(new FileOutputStream("result.html"))) {
+        result = result.replace("$handler", selectHandler);
+        try (PrintStream out = new PrintStream(new FileOutputStream(cli.outputFile()))) {
             out.print(result);
         }
     }
 
-    private static Map<String, StringBuffer> generate(File file) throws IOException {
+    private Map<String, StringBuffer> generate(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line = reader.readLine();
 
@@ -99,25 +118,29 @@ public class Metrics {
             if (line.contains("Metric")) {
 
 
-
 //                if (line.contains(metric)) {
 //                    lineCount++;
 //                    if(lineCount>5){
 //                        break;
 //                    }
                 String[] split = line.split(" ");
-                String time = split[2];
 
-                String[] metricKeyValue = split[3].split("=");
-                String metric = metricKeyValue[0].substring(7);
-                String value = metricKeyValue[1];
 
-                StringBuffer sb = map.computeIfAbsent(metric, k -> new StringBuffer());
+                long time = Long.parseLong(split[2]);
+                if (cli.fromEpoch() <= time && cli.toEpoch() >= time) {
 
-                value = value.substring(0, value.length() - 1);
-                System.out.println(metric);
+                    String[] metricKeyValue = split[3].split("=");
+                    String metric = metricKeyValue[0].substring(7);
+                    String value = metricKeyValue[1];
 
-                //if(sb.length()<100) {
+                    StringBuffer sb = map.computeIfAbsent(metric, k -> new StringBuffer());
+
+                    value = value.substring(0, value.length() - 1);
+
+
+                    // System.out.println(metric);
+
+                    //if(sb.length()<100) {
 
                     if (sb.length() > 0) {
                         sb.append(",");
@@ -125,7 +148,8 @@ public class Metrics {
 
                     sb.append("[new Date(").append(time).append("),").append(value).append("]");
                     sb.append("\n");
-                //}
+                    //}
+                }
             }
             line = reader.readLine();
         }
---
+++ src/main/java/com/hazelcast/MetricsCli.java
@@ -0,0 +1,69 @@
+package com.hazelcast;
+
+import joptsimple.NonOptionArgumentSpec;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class MetricsCli {
+
+    private final OptionParser parser = new OptionParser();
+
+    private final OptionSpec<Long> fromEpochSpec = parser.accepts("from",
+            "The epoch time to start from.")
+            .withRequiredArg().ofType(Long.class).defaultsTo(0L);
+
+    private final OptionSpec<Long> toEpochSpec = parser.accepts("to",
+            "The epoch time to end.")
+            .withRequiredArg().ofType(Long.class).defaultsTo(Long.MAX_VALUE);
+
+    private final OptionSpec<String> resultSpec = parser.accepts("result",
+            "The epoch time to end.")
+            .withRequiredArg().ofType(String.class).defaultsTo("result.html");
+
+//    private final NonOptionArgumentSpec<String> nonOptionArgumentSpec = parser.nonOptions().ofType(String.class);
+
+    private final String[] args;
+    private final OptionSet optionSet;
+
+    public MetricsCli(String[] args) {
+        this.args = args;
+        optionSet = parser.parse(args);
+    }
+
+    public long fromEpoch() {
+        return optionSet.valueOf(fromEpochSpec);
+    }
+
+    public long toEpoch() {
+        return optionSet.valueOf(toEpochSpec);
+    }
+
+    public File inputFile() throws IOException {
+        List nonOptionArguments = optionSet.nonOptionArguments();
+        if (nonOptionArguments.size() != 1) {
+            throw new IllegalArgumentException("One argument expected");
+        }
+
+        File file = new File((String)nonOptionArguments.get(0));
+        if (!file.exists()) {
+            throw new IOException(format("Input file '%s' doesn't exist", file));
+        }
+
+        return file;
+    }
+
+    public File outputFile() throws IOException {
+        File file = new File(optionSet.valueOf(resultSpec));
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        return file;
+    }
+}
